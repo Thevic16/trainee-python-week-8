b098fb9abc1d1ed117e626a3358e7aafd15fb815
Víctor Gómez <vgomez@applaudostudios.dev>
2022-03-23 17:59:04 -0400

    Intro create relations between models

--- my_app/film/models.py
+++ my_app/film/models.py
@@ -8,3 +8,29 @@ class Category(db.Model):
 
     def __repr__(self):
         return self.name
+
+
+class Film(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), unique=True)
+    description = db.Column(db.Text, nullable=True)
+    release_date = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship(
+        'Category', backref=db.backref('films', lazy='dynamic')
+    )
+    price_by_day = db.Column(db.Float)
+    stock = db.Column(db.Integer)
+    availability = db.Column(db.Integer)
+    film_type = db.Column(db.String(120))
+    # Remember add image column
+
+    # One to One relationship
+    '''
+        film_prequel = db.relationship(
+        'Film', uselist=False
+        )
+    '''
+
+    def __repr__(self):
+        return self.title
--- my_app/film/views.py
+++ my_app/film/views.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify, Blueprint
 from my_app import db
-from my_app.film.models import Category
+from my_app.film.models import Category, Film
 
 film = Blueprint('film', __name__)
 
@@ -38,3 +38,46 @@ def categories():
         db.session.add(category)
         db.session.commit()
         return f'Category create: category: {category}'
+
+
+@film.route('/api/films/', methods=['GET', 'POST'])
+def films():
+    if request.method == 'GET':
+        films = Film.query.all()
+        res = {}
+        for film in films:
+            res[film.id] = {
+                'title': film.title,
+                'description': film.description,
+                'release_date': film.release_date,
+                'category': film.category.id,
+                'price_by_day': film.price_by_day,
+                'stock': film.stock,
+                'availability': film.availability,
+                'film_type': film.film_type
+                # 'film_prequel': film.film_prequel.title
+            }
+        return jsonify(res)
+    elif request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        release_date = request.form.get('release_date')
+        category_id = int(request.form.get('category_id'))
+        price_by_day = request.form.get('price_by_day')
+        stock = request.form.get('stock')
+        availability = request.form.get('availability')
+        film_type = request.form.get('film_type')
+        # film_prequel = request.form.get('film_prequel')
+
+        category = Category.query.get_or_404(category_id)
+
+        film = Film(title=title, description=description,
+                    release_date=release_date, category_id=category_id,
+                    category=category, price_by_day=price_by_day,
+                    stock=stock, availability=availability,
+                    film_type=film_type)
+        # film_prequel=film_prequel)
+
+        db.session.add(film)
+        db.session.commit()
+        return f'film create: film: {film}'

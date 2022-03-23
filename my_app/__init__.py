0e28691a2ed12d6e01f3723ad728f1c50e1b1e1a
Víctor Gómez <vgomez@applaudostudios.dev>
2022-03-23 10:11:07 -0400

    Add environments variables

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,5 +1,13 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+db = SQLAlchemy(app)
 
 import my_app.film.views
--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 from my_app import app
-app.run(port=5000, debug=True)
+import os
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env.
+
+if os.getenv('DEBUG_STATE') == 'True':
+    app.run(port=os.getenv('PORT'), debug=True)
+elif os.getenv('DEBUG_STATE') == 'False':
+    app.run(port=os.getenv('PORT'), debug=False)

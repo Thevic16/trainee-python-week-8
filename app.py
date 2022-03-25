34eb42bc1aedff66263e721c79aa07812c93764a
Víctor Gómez <vgomez@applaudostudios.dev>
2022-03-24 21:42:00 -0400

    Add jwt library and secret key

--- app.py
+++ app.py
@@ -9,17 +9,22 @@ from marshmallow import ValidationError
 import os
 from dotenv import load_dotenv
 
+# Load virtual variables ------------------------------------------------------
 load_dotenv()  # take environment variables from .env.
 
+# Initializing Flask app ------------------------------------------------------
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
 bluePrint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(bluePrint, doc='/doc/', title='Film Rental System Application')
 app.register_blueprint(bluePrint)
 
+# Database configuration ------------------------------------------------------
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['PROPAGATE_EXCEPTIONS'] = True
 
+# Add namespaces --------------------------------------------------------------
 api.add_namespace(category_ns)
 api.add_namespace(categories_ns)
 
@@ -34,10 +39,11 @@ def handle_validation_error(error):
     return jsonify(error.messages), 400
 
 
+# Defining resources ----------------------------------------------------------
 category_ns.add_resource(CategoryResource, '/<int:id>/')
 categories_ns.add_resource(CategoryResourceList, "/")
 
-# Run server
+# Run server ------------------------------------------------------------------
 db.init_app(app)
 ma.init_app(app)
 if os.getenv('DEBUG_STATE') == 'True':

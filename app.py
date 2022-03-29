018531e79bffe18203fc40038e9547caa7cc034a
Víctor Gómez <vgomez@applaudostudios.dev>
2022-03-28 22:44:31 -0400

    Add finals details

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ api = Api(bluePrint, doc='/doc/', title='Film Rental System Application')
 app.register_blueprint(bluePrint)
 
 # Database configuration ------------------------------------------------------
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['PROPAGATE_EXCEPTIONS'] = True
 
